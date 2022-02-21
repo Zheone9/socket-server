1d59a326bc4ac242e8b024bdbad1f14f82b7da36
Jorge <83678281+krRyptt0m01@users.noreply.github.com>
2022-02-21 00:50:47 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+import Server from "./classes/server";
+import { SERVER_PORT } from "./global/environment";
+import router from "./routes/router";
+import bodyParser from "body-parser";
+
+const server = new Server();
+//bodyParser
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
+
+server.app.use("/", router);
+
+server.start(() => {
+  console.log(`Servidor corriendo en el puerto ${SERVER_PORT}`);
+});
