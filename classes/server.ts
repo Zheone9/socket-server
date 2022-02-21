8b3fa7fc2666a09771f832143fbfdab4953b4a35
Jorge <83678281+krRyptt0m01@users.noreply.github.com>
2022-02-21 11:50:03 -0500

    Cors arreglado

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,40 @@
 import express from "express";
 import { SERVER_PORT } from "../global/environment";
+import { Server as ServerIO } from "socket.io";
+import cors from "cors";
+import http from "http";
 
 export default class Server {
+  private static _instance: Server;
   public app: express.Application;
   public port: number;
 
-  constructor() {
+  public io: ServerIO;
+  private httpServer: http.Server;
+
+  private constructor() {
     this.app = express();
+    this.app.use(cors());
     this.port = SERVER_PORT;
+    this.httpServer = new http.Server(this.app);
+    this.io = new ServerIO(this.httpServer, {
+      cors: {
+        origin: ["http://localhost:4200"],
+      },
+    });
+    this.escucharSockets();
+  }
+  private escucharSockets() {
+    console.log("escuchando conexiones");
+    this.io.on("connection", (cliente) => {
+      console.log("Nuevo cliente");
+    });
   }
 
+  public static get instance() {
+    return this._instance || (this._instance = new this());
+  }
   start(callback: () => void) {
-    this.app.listen(this.port, callback);
+    this.httpServer.listen(this.port, callback);
   }
 }
